fdc9fa2a1eaef1f3b36e0b50bb39e79b5dd75a87
simonedc97 <simonedicarlo97@gmail.com>
2026-01-21 17:46:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -659,11 +659,15 @@ with tab_stress:
     
     # Nota sotto il grafico
     st.markdown(
-        "<sub>Note: the shaded areas represent the dispersion between the 25th and 75th percentile of the Bucket.</sub>",
+        """
+        <div style="display: flex; align-items: center;">
+            <div style="width: 20px; height: 14px; background-color: rgba(255,0,0,0.25); margin-right: 8px; border: 1px solid rgba(0,0,0,0.1);"></div>
+            <sub>Note: the shaded areas represent the dispersion between the 25th and 75th percentile of the Bucket.</sub>
+        </div>
+        """,
         unsafe_allow_html=True
     )
 
-    
     # -----------------------------
     # Bottone di download Excel dei dati del grafico
     # -----------------------------
