dc4b5b016909c39a9c0ff60624975ad6f3d8ffbe
simonedc97 <simonedicarlo97@gmail.com>
2026-01-21 16:17:13 +0100

    Comment out title in figure layout
    Comment out the title in the figure layout.

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ with tab_stress:
         )
         
         fig.update_layout(
-            title=f"Comparison Analysis – {selected_portfolio} on {selected_date.strftime('%Y/%m/%d')}",
+            #title=f"Comparison Analysis – {selected_portfolio} on {selected_date.strftime('%Y/%m/%d')}",
             xaxis_title="Stress PnL (bps)",
             yaxis_title="Scenario",
             template="plotly_white",
